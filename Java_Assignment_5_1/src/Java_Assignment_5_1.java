07b17eb5f4d9b37cc77987a07447f309d7b0ff62
D4_62758_PratikSatpute <pratiksatpute17@gmail.com>
2022-04-01 18:43:13 +0530

    assignment5_1

---
+++ Java_Assignment_5_1/src/Java_Assignment_5_1.java
@@ -0,0 +1,14 @@
+/*
+ Write a java program to reverse a String
+ */
+public class Java_Assignment_5_1 
+{
+	public static void main(String[]args)
+	{
+		StringBuffer str1= new StringBuffer("HEllo, World!");
+		str1.reverse();
+		System.out.println(str1);
+		
+	}
+
+}
